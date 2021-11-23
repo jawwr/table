00a2ff61b0c34570aee64d9cbe4f8cf50a29abe4
Arseny <20mineslend14@gmail.com>
2021-11-23 23:26:33 +0500

    оно работает

--- Table/Program.cs
+++ Table/Program.cs
@@ -18,23 +18,12 @@ namespace Table
             int[] value = GenerateRandomValue(n, m);
             int width = GetMaxWidth(value);
 
-
             foreach (var str in GenerateTable(m,n,value,width))
             {
                 Console.WriteLine(str);
             }
-            // Console.WriteLine(width);
             
-            // Console.WriteLine('╔' + PrintSymbols('═', width) + '╦' + PrintSymbols('═', width) + '╦' + PrintSymbols('═', width) + '╗');
-            //
-            // Console.WriteLine('║' + '║' + '║');
-            // int number = 1;
-                
-            // Console.WriteLine('╠' + PrintSymbols('═', width) + '╬' + PrintSymbols('═', width) + '╬' + PrintSymbols('═', width) + '╣');
-               
-            // Console.WriteLine('║' + PutInCell((number++).ToString(), width) + '║' + PutInCell(((float)Math.Round(i, 4)).ToString(), width) + '║' + PutInCell(GetEncountedValue(i), width) + '║');
-            
-            // Console.WriteLine('╚' + PrintSymbols('═', width) + '╩' + PrintSymbols('═', width) + '╩' + PrintSymbols('═', width) + '╝');
+            Console.Read();
         }
 
         static int[] GenerateRandomValue(int n, int m)
@@ -68,10 +57,17 @@ namespace Table
             return result;
         }
  
-        // static string PrintValue(int word, int width) {
-        //     int count = width - word.Length - 2;
-        //     return ' ' + word + PrintSymbols(' ', count) + ' ';
-        // }
+        static string PrintValue(int word, int width) {
+            int count = width - word.ToString().Length;
+            string str = string.Empty;
+            if (count == 1)
+                str = $"{word} ";
+            else if (count == 2)
+                str = $"{word}  ";
+            else
+                str = $"{word}";
+            return str;
+        }
 
         static string[] GenerateTable(int width, int height, int[] values, int widthmax)
         {
@@ -80,7 +76,8 @@ namespace Table
             for (int i = 0; i < width; i++)
                 strings[0] += i != width - 1 ? $"{PrintSymbols('═', widthmax)}╦" : $"{PrintSymbols('═', widthmax)}";
             strings[0] += "╗";
-            for (int i = 1; i < height + (height - 1) - 1; i++)
+            int c = 0;
+            for (int i = 1; i <= height + (height - 1); i++)
             {
                 if (i % 2 == 0)
                 {
@@ -89,32 +86,22 @@ namespace Table
                         strings[i] += j != width - 1 ? $"{PrintSymbols('═', widthmax)}╬" : $"{PrintSymbols('═', widthmax)}";
                     strings[i] += "╣";
                 }
-                // else
-                // {
-                //     strings[i] = "║";
-                //     for (int j = 0; j < width; j++)
-                //     {
-                //         strings[i] += i % 1 == 0 ? $"{PrintSymbols(' ',widthmax)}║" : "num║";
-                //     }
-                //     strings[i] += "║";
-                // }
-                
+                else
+                {
+                    strings[i] = "║";
+                    for (int j = 0; j < width; j++)
+                    {
+                        strings[i] += j != width - 1 ?$"{PrintValue(values[c],widthmax)}║" : $"{PrintValue(values[c],widthmax)}";
+                        c++;
+                    }
+                    strings[i] += "║";
+                }
             }
             strings[strings.Length - 1] = "╚";
             for (int i = 0; i < width; i++)
                 strings[strings.Length - 1] += i != width - 1 ? $"{PrintSymbols('═', widthmax)}╩" : $"{PrintSymbols('═', widthmax)}";
             strings[strings.Length - 1] += "╝";
             return strings;
-        }
-        
-        static string GenerateColumn(int widthmax, int heightmax, int width)
-        {
-            string str = string.Empty;
-            for (int i = 0; i < width; i++)
-            {
-                str += i != width ? $"{PrintSymbols('═', widthmax)}╦" : $"";
-            }
-            return $"{PrintSymbols('═', widthmax)}╦{PrintSymbols('═', widthmax)}╦{PrintSymbols('═', widthmax)}";
         }
     }
 }
